f6b5c736babe5cc232e4ed824875fb76b6a5af87
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-28 05:23:13 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var JSX =
+React.createElement('div', null,
+  React.createElement('h1', null, 'Hello World'),
+  React.createElement('p', null, 'Lets render this to the DOM'));
+
+
+
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
